622152f7df4bd2f94172e45d3abc96fe46bf9e10
Yunze Xu <xyzinfernity@163.com>
2020-10-18 23:02:36 +0800

    Fix return type error

--- modern-cpp/cpp17/library/string_view.cc
+++ modern-cpp/cpp17/library/string_view.cc
@@ -6,7 +6,7 @@ using namespace std;
 // 使用 string_view 保存分割结果，每个 string_view
 // 仅保存两个指针，因此不会有复制整个字符串的开销
 inline auto splitToViews(const std::string& s, const std::string& separator) {
-  std::vector<std::string> tokens;
+  std::vector<std::string_view> tokens;
   size_t pos = 0;
   while (true) {
     if (auto new_pos = s.find(separator, pos); new_pos != std::string::npos) {
