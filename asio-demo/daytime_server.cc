d54d9e791b798b538ef7a840dea67b5d98146427
Yunze Xu <xyzinfernity@163.com>
2021-06-15 21:27:51 +0800

    Make daytime server asynchronous

--- asio-demo/daytime_server.cc
+++ asio-demo/daytime_server.cc
@@ -1,5 +1,7 @@
 #include <boost/asio.hpp>
 #include <ctime>
+#include <iostream>
+#include <memory>
 #include <string>
 
 using boost::asio::ip::tcp;
@@ -9,18 +11,67 @@ inline std::string make_daytime_string() {
   return std::ctime(&now);
 }
 
+class TcpConnection : public std::enable_shared_from_this<TcpConnection> {
+ public:
+  static auto create(boost::asio::io_context& io_context) {
+    return std::shared_ptr<TcpConnection>(new TcpConnection(io_context));
+  }
+
+  auto& socket() { return socket_; }
+
+  void start() {
+    message_ = make_daytime_string();
+
+    auto self = shared_from_this();
+    boost::asio::async_write(
+        socket_, boost::asio::buffer(message_),
+        [this, self](const boost::system::error_code& error,
+                     std::size_t bytes_transferred) {
+          if (error) {
+            std::cerr << "[ERROR] Failed to write: " << error << std::endl;
+          } else {
+            std::cout << "[DEBUG] write " << bytes_transferred
+                      << " bytes to client" << std::endl;
+          }
+        });
+  }
+
+ private:
+  TcpConnection(boost::asio::io_context& io_context) : socket_(io_context) {}
+
+  tcp::socket socket_;
+  std::string message_;
+};
+
+class TcpServer {
+ public:
+  TcpServer(boost::asio::io_context& io_context)
+      : io_context_(io_context),
+        acceptor_(io_context_, tcp::endpoint(tcp::v4(), 13)) {
+    startAccept();
+  }
+
+ private:
+  boost::asio::io_context& io_context_;
+  tcp::acceptor acceptor_;
+
+  void startAccept() {
+    const auto new_connection = TcpConnection::create(io_context_);
+
+    acceptor_.async_accept(
+        new_connection->socket(),
+        [this, new_connection](const boost::system::error_code& error) {
+          if (!error) {
+            new_connection->start();
+          }
+          startAccept();
+        });
+  }
+};
+
 int main(int argc, char* argv[]) {
   boost::asio::io_context io_context;
-  tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
-
-  for (;;) {
-    tcp::socket socket(io_context);
-    acceptor.accept(socket);
-
-    const auto message = make_daytime_string();
-
-    boost::system::error_code ignored_error;
-    boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
-  }
+  TcpServer server(io_context);
+  io_context.run();
   return 0;
 }
